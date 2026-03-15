248f4d8609f3ffc7fceda4be48f569b2d1f4b105
Kelptalks <110746221+Kelptalks@users.noreply.github.com>
2026-03-15 10:31:21 -0400

    Fixes scaling of play view widgert

--- src/game_data/screen/widget/world_rendering/play_world_view_render.rs
+++ src/game_data/screen/widget/world_rendering/play_world_view_render.rs
@@ -1,9 +1,8 @@
 use std::{cell::RefCell, rc::Rc, sync::{Arc, RwLock}};
 
-use crate::game_data::{TextureManager, World, game_event_manager::game_event_manager::GameEventManager, screen::{ScreenData, camera_data::Direction, iso_cord_tool, widget::{panel::panel::Panel, widget::Widget, world_rendering::play_block::PlayBlock}}, types::BlockTexture};
+use miniquad::KeyCode;
 
-static MAX_VIEW_DISTANCE: i32 = 20;
-static MIN_VIEW_DISTANCE: i32 = 0;
+use crate::game_data::{TextureManager, World, game_event_manager::game_event_manager::GameEventManager, screen::{ScreenData, camera_data::Direction, iso_cord_tool, widget::{panel::panel::Panel, widget::Widget, widget_calculations, world_rendering::play_block::PlayBlock}}, types::BlockTexture};
 
 /*
 ###############
@@ -56,9 +55,19 @@ impl ViewDirection {
 }
 
 pub struct PlayWorldViewRender {
-    panel: Panel,
-    // Parent
+    // Parent rendering
+    parent_pos: [f32; 4],
+    parent_scale: [f32; 2],
     prefered_scale: [f32; 2],
+
+    // Self Rendering
+    external_buffers: [f32; 4],  
+    internal_buffers: [f32; 4],
+    pos: [f32; 4],
+    scale: [f32; 2],
+
+    center_ndc: [f32; 2],
+
 
     // World Rendering
     world_ref: Option<Arc<RwLock<World>>>,
@@ -78,11 +87,20 @@ pub struct PlayWorldViewRender {
 
 impl PlayWorldViewRender {
     pub fn new(parent_pos: [f32; 4], buffers: [f32; 4]) -> PlayWorldViewRender{
-        let mut panel = Panel::new(parent_pos, buffers);
-        panel.add_text_display("Play View".to_string());
-        PlayWorldViewRender {
-            panel: panel,
-            prefered_scale: [0.0; 2],
+        PlayWorldViewRender {            
+           // Parent Rendering
+            parent_pos: parent_pos,
+            parent_scale: [0.0; 2],
+            prefered_scale: [1.0; 2],
+
+            // Self Rendering
+            external_buffers: buffers, 
+            internal_buffers: [0.012; 4],   
+            pos: [0.0; 4],
+            scale: [0.0; 2],
+
+            center_ndc: [0.0; 2],
+
 
             // Player Data Links
             world_ref: None,
@@ -154,17 +172,28 @@ impl PlayWorldViewRender {
     // Rendering
     //=====================================
 
-    pub fn update_rendering_scales(&mut self) {
-        self.ndc_block_scale = self.render_scale / ((self.zoom * 2) + 1) as f32;
+    pub fn size(&mut self) {
+        self.parent_scale = widget_calculations::pos_to_scale(self.parent_pos);
+        self.pos = widget_calculations::buffer_pos(self.parent_pos, self.external_buffers);
+        self.scale = widget_calculations::pos_to_scale(self.pos);
+        
+        self.center_ndc = [
+            self.pos[0] + (self.scale[0] / 2.0),
+            self.pos[1] + (self.scale[1] / 2.0),
+        ];
+
+
+        self.ndc_block_scale = (self.scale[0] / ((self.zoom * 2) + 1) as f32) / 2.0;
         self.ndc_tile_scale = self.ndc_block_scale / 2.0;
         self.ndc_tile_half_scale = self.ndc_tile_scale / 2.0;
+
     }
 
     pub fn render_view(&mut self, 
         screen_data: &ScreenData, 
         texture_manager: &mut TextureManager,
     ) {
-        self.update_rendering_scales();
+        self.size();
         self.handle_camera_panning(screen_data);
 
         let world;
@@ -176,7 +205,8 @@ impl PlayWorldViewRender {
         }
 
 
-        let ndc_x_draw_center_offset = self.ndc_block_scale;
+        let ndc_x_draw_center_offset = self.center_ndc[0] + self.ndc_block_scale;
+        let ndc_y_draw_center_offset = self.center_ndc[1] - self.ndc_block_scale;
 
         let rot = self.camera_direction.rotation_matrix();
         
@@ -204,7 +234,10 @@ impl PlayWorldViewRender {
                     
                     // Draw Cords
                     let mut draw_cords = iso_cord_tool::casted_to_ndc_cords(self.ndc_block_scale, [x - z, y - z]);
+                    
                     draw_cords[0] -= ndc_x_draw_center_offset;
+                    draw_cords[1] += ndc_y_draw_center_offset;
+                    
                     draw_cords[0] += self.camera_ndc_offset[0];
                     draw_cords[1] += self.camera_ndc_offset[1];
 
@@ -256,35 +289,39 @@ impl PlayWorldViewRender {
     }
 
 
+    //=====================================
+    // Settings
+    //=====================================
 
+    pub fn set_prefered_size(&mut self, scale: f32) {
+        self.parent_scale = [scale; 2];
+    }
 
 }
 
 impl Widget for PlayWorldViewRender {
     fn get_pos(&self) -> [f32; 4] {
-        self.panel.get_pos()
+        return self.pos;
     }
 
     fn get_scale(&self) -> [f32; 2] {
-        self.panel.get_scale()
+        return self.scale;
     }
 
     fn get_prefered_scale(&self) -> [f32; 2] {
-        return self.panel.get_prefered_scale();
+        return self.prefered_scale;
     }
 
     fn set_buffers(&mut self, pos: [f32; 4]) {
-        self.panel.set_buffers(pos);
+        self.external_buffers = pos;
     }
 
     fn set_parent_pos(&mut self, pos: [f32; 4]) {
-        self.panel.set_parent_pos(pos);
+        self.parent_pos = pos;
     }
 
     fn size(&mut self) {
-        self.panel.size();
-
-        // self.prefered_scale = [self.ndc_block_scale * self.zoom as f32 * 2.0; 2];
+        self.size();
     }
 
     fn render(
@@ -293,9 +330,38 @@ impl Widget for PlayWorldViewRender {
         screen_data: &ScreenData, 
         game_event_manager: &mut GameEventManager
     ) {
+        texture_manager.render_ui_element_with_pos(crate::game_data::types::UITextures::MirrorBackground, self.pos);
         
         self.render_view(screen_data, texture_manager);
 
-        self.panel.render(texture_manager, screen_data, game_event_manager);
+
+        if let Some(cords_ref) = &self.camera_cords_ref {
+            let inputs = screen_data.get_inputs();
+            for input in inputs {
+                match input {
+                    crate::game_data::screen::input_data::Input::KeyDown(key_code) => {
+                        if *key_code == KeyCode::LeftShift {
+                            cords_ref.borrow_mut()[2] -= 1;
+                        }
+                        else if *key_code == KeyCode::Space {
+                            cords_ref.borrow_mut()[2] += 1;
+                        }
+                    },
+                    crate::game_data::screen::input_data::Input::MouseWheel(x, y) => {
+                        if *y > 0.0 {
+                            self.zoom += 1;
+                        }
+                        else if *y < 0.0 { 
+                            self.zoom -= 1;
+                        }
+                    },
+                    _ => {
+
+                    }
+
+                }
+            }
+        }
+
     }
 }
