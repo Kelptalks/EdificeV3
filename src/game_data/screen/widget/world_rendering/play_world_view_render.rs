eb3471074b7edb1c5cf81a7837c80bc9579f6684
Kelptalks <110746221+Kelptalks@users.noreply.github.com>
2026-03-22 21:31:32 -0400

    fix rendering cord mismatch

--- src/game_data/screen/widget/world_rendering/play_world_view_render.rs
+++ src/game_data/screen/widget/world_rendering/play_world_view_render.rs
@@ -242,6 +242,7 @@ impl PlayWorldViewRender {
             *axis += self.rendering_config.get_zoom();
         }
 
+        println!("Start: {:?} | End: {:?}", start_cords, end_cords);
         return [start_cords, end_cords];
     }
 
@@ -270,9 +271,9 @@ impl PlayWorldViewRender {
         let end_cords = area_to_render[1];
 
         // Loop through blocks in zoom
-        for z in start_cords[0]..=end_cords[0] {
+        for z in start_cords[2]..=end_cords[2] {
             for y in start_cords[1]..=end_cords[1] {
-                for x in start_cords[2]..=end_cords[2] {
+                for x in start_cords[0]..=end_cords[0] {
                     let world_block_cords = [
                         camera_cords[0] + (rot[0][0] * x + rot[0][1] * y),
                         camera_cords[1] + (rot[1][0] * x + rot[1][1] * y),
@@ -286,36 +287,36 @@ impl PlayWorldViewRender {
                     ];
 
 
-                    if !BlockTexture::from_id(world.get_world_value(blocking_block_cords)).is_opaque() {
-                        // Block Type
-                        let block_type_at_cord = BlockTexture::from_id(world.get_world_value(world_block_cords));
-                        
-                        // Draw Cords
-                        let mut draw_cords = iso_cord_tool::casted_to_ndc_cords(self.ndc_block_scale, [x - z, y - z]);
-                        
-                        draw_cords[0] += ndc_x_draw_center_offset;
-                        draw_cords[1] += ndc_y_draw_center_offset;
-                        
-                        draw_cords[0] += self.camera_ndc_offset[0];
-                        draw_cords[1] += self.camera_ndc_offset[1];
+                    
+                    // Block Type
+                    let block_type_at_cord = BlockTexture::from_id(world.get_world_value(world_block_cords));
+                    
+                    // Draw Cords
+                    let mut draw_cords = iso_cord_tool::casted_to_ndc_cords(self.ndc_block_scale, [x - z, y - z]);
+                    
+                    draw_cords[0] += ndc_x_draw_center_offset;
+                    draw_cords[1] += ndc_y_draw_center_offset;
+                    
+                    draw_cords[0] += self.camera_ndc_offset[0];
+                    draw_cords[1] += self.camera_ndc_offset[1];
 
-                        // Create a play block if block needs to be rendered
-                        let mut play_block = PlayBlock::new_blank();
+                    // Create a play block if block needs to be rendered
+                    let mut play_block = PlayBlock::new_blank();
 
-                        // World
-                        play_block.block_type = block_type_at_cord;
-                        play_block.block_world_cords = world_block_cords;
+                    // World
+                    play_block.block_type = block_type_at_cord;
+                    play_block.block_world_cords = world_block_cords;
 
-                        // Rendering
-                        play_block.rendering_block_cords = [x, y, z];
-                        play_block.draw_cords = draw_cords;
-                        play_block.ndc_block_scale = self.ndc_block_scale;
+                    // Rendering
+                    play_block.rendering_block_cords = [x, y, z];
+                    play_block.draw_cords = draw_cords;
+                    play_block.ndc_block_scale = self.ndc_block_scale;
 
 
-                        play_block.render_block(texture_manager);
-                        play_block.render_cursor(texture_manager, self.rendering_config.get_block_ghost());
-                        play_block.render_area(texture_manager, self.rendering_config.get_location_ref().borrow().get_area());
-                    }
+                    play_block.render_block(texture_manager);
+                    play_block.render_cursor(texture_manager, self.rendering_config.get_block_ghost());
+                    play_block.render_area(texture_manager, self.rendering_config.get_location_ref().borrow().get_area());
+                    
                 }
             }
         }
